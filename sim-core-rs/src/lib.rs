a46a50d87505ceab6acebd1971e895d2eed537d8
Jonathan Tynan <jojaty@gmail.com>
2025-09-28 10:38:16 +1000

    feat(core): add initial drivetrain impl

--- sim-core-rs/src/lib.rs
+++ sim-core-rs/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Drivetrain {
+    rpm: f32,
+    speed: f32,
+    gear: i32,
+    throttle: f32,
+    clutch: f32,
+    engine_on: bool,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+impl Drivetrain {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Drivetrain {
+        Drivetrain {
+            rpm: 900.0,
+            speed: 0.0,
+            gear: 0,       // 0 == neutral
+            throttle: 0.0, // 0..1
+            clutch: 0.0,   // 0..1, 1=fully engaged
+            engine_on: true,
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn rpm(&self) -> f32 {
+        self.rpm
+    }
+    pub fn speed_kmh(&self) -> f32 {
+        self.speed * 3.6
+    }
+    pub fn gear(&self) -> i32 {
+        self.gear
+    }
+    pub fn throttle(&self) -> f32 {
+        self.throttle
+    }
+    pub fn clutch(&self) -> f32 {
+        self.clutch
+    }
+    pub fn engine_on(&self) -> bool {
+        self.engine_on
     }
 }
